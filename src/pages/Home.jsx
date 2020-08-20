9cfb430c4b8871a1bf1da669325df2c73e834997
Santiago Forero <ds.foreroa@uniandes.edu.co>
2020-08-20 15:15:41 -0500

    cards in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,6 +23,14 @@ export class Home extends PureComponent {
           productName="productName"
           photoURL="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/o/soundlink_around_ear_wireless_ii_hr14_1.jpg"
         />
+        <ProductCard
+          productName="productName"
+          photoURL="https://ishopcolombia.vteximg.com.br/arquivos/ids/180099-1000-1000/MWP22AM_A_3.jpg?v=637091799183200000"
+        />
+        <ProductCard
+          productName="productName"
+          photoURL="https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/7/2/723755138414_15.jpg"
+        />
       </section>
     );
   }
