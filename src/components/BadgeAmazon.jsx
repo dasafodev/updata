30aaee79d5e2681a91e480f512e49f3987fb1297
Josh Wolf <alienxtrix@gmail.com>
2020-08-26 12:56:54 -0500

    Categories started

---
+++ src/components/BadgeAmazon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class BadgeAmazon extends React.Component{
+    render(){
+        return(
+        <div>
+            <img></img>
+            
+        </div>
+        )
+    }
+}
---
+++ src/pages/Categories.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/header';
+
+import '../styles/components/Home.css';
+
+class Categories extends React.Component{
+    render(){
+        return(
+            <div>
+        <Header />
+        </div>
+        )
+    }
+}
+
+export default Categories;
