23d019b6b6bd6091dd2e8460a77a20a7ca563b37
Mono <al_unlimited@hotmail.com>
2020-08-27 08:11:10 -0500

    14 - agregando estilos componente producto

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,56 +6,63 @@ export class Products extends PureComponent {
     render() {
     return (
 <section className="product-list">
-          <Product
-            productName="productName"
+    <Product
+        productName="productName"
             productURL="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/p/ipadair-silver-wifi_1.jpg"
             nameProduct="Ipad Mini"
             priceProduct="25000"
             ratingProduct="5🌟"
-          />
-                    <Product
+        />
+
+        <Product
             productName="productName"
             productURL="https://exitocol.vtexassets.com/arquivos/ids/548642/smartwatch-reloj-inteligente-sumergible-control-musica-rosa.jpg?v=637012391578770000"
             nameProduct="Ipad Mini"
             priceProduct="25000"
+            categorieProduct="Tablets"
             ratingProduct="5🌟"
-          />
-                    <Product
+        />
+
+        <Product
             productName="productName"
             photoURL="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/p/ipadair-silver-wifi_1.jpg"
             nameProduct="Ipad Mini"
             priceProduct="25000"
             ratingProduct="5🌟"
-          />
-                    <Product
+        />
+
+        <Product
             productName="productName"
             photoURL="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/p/ipadair-silver-wifi_1.jpg"
             nameProduct="Ipad Mini"
             priceProduct="25000"
             ratingProduct="5🌟"
-          />
-                    <Product
+        />
+
+        <Product
             productName="productName"
             photoURL="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/p/ipadair-silver-wifi_1.jpg"
             nameProduct="Ipad Mini"
             priceProduct="25000"
             ratingProduct="5🌟"
-          />
-                    <Product
+        />
+
+        <Product
             productName="productName"
             photoURL="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/p/ipadair-silver-wifi_1.jpg"
             nameProduct="Ipad Mini"
             priceProduct="25000"
             ratingProduct="5🌟"
-          />
-                    <Product
+        />
+
+        <Product
             productName="productName"
             photoURL="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/p/ipadair-silver-wifi_1.jpg"
             nameProduct="Ipad Mini"
             priceProduct="25000"
             ratingProduct="5🌟"
-          />
-          </section>
+        />
+        </section>
 		);
 	}
 }
