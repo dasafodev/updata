f04ff7a7500b4b5b10a83e5fe6392a87bf844c89
Mono <al_unlimited@hotmail.com>
2020-08-27 19:12:30 -0500

    16 - agregando estilos componente producto

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Logo from '../assets/logo.png';
 
@@ -27,33 +26,3 @@ export default function Header() {
     </div>
   );
 }
-=======
-import React from 'react';
-import Logo from '../assets/logo.png';
-
-import '../styles/components/Header.css';
-import { Link } from 'react-router-dom';
-
-export default function Header() {
-  return (
-    <div className="header">
-      <div className="wrapper">
-        <div className="logo">
-          <img
-            src={Logo}
-            alt="logo"
-          />
-          UpData
-        </div>
-
-        <nav>
-            <Link to="/Home">Inicio</Link>
-            <Link to="/Products">Productos</Link>
-            <Link to="/Categories">Categorias</Link>
-            <Link>Nosotros</Link>
-        </nav>
-      </div>
-    </div>
-  );
-}
->>>>>>> 23d019b6b6bd6091dd2e8460a77a20a7ca563b37
