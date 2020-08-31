0e70956660a9b01e3a593e122e76678c8d90eeb9
Josh Wolf <alienxtrix@gmail.com>
2020-08-31 16:03:47 -0500

    fix errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import '../styles/components/App.css';
 import { Home } from '../pages/Home';
@@ -28,32 +28,3 @@ function TestPage()
 }
 
 export default App;
-=======
-import React from 'react';
-import '../styles/components/App.css';
-import { Home } from '../pages/Home';
-import Layout from './Layout';
-import { Categories } from '../pages/Categories';
-import { Products } from '../pages/Products';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-const App = () => (
-    <BrowserRouter>
-        <Layout>
-            <Switch>
-                <Route exact path="/home" component={Home} />
-                <Route exact path="/" component={TestPage} />
-                <Route exact path="/products" component={Products} />
-                <Route exact path="/categories" component={Categories} /> 
-            </Switch>
-        </Layout>
-
-    </BrowserRouter>
-);
-function TestPage()
-{
-    return <h1>Test</h1>
-}
-
-export default App;
->>>>>>> 23d019b6b6bd6091dd2e8460a77a20a7ca563b37
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import BadgeAmazon from '../components/BadgeAmazon';
 import BadgeMl from '../components/BadgeMl';
@@ -20,28 +19,4 @@ export class Categories extends React.Component{
     }
 }
 
-=======
-import React from 'react';
-import Header from '../components/header';
-import BadgeAmazon from '../components/BadgeAmazon';
-import BadgeMl from '../components/BadgeMl';
-
-import '../styles/components/Home.css';
-
-export class Categories extends React.Component{
-    render(){
-        return(
-            <div className = "cat-container">
-            <div>
-        <BadgeAmazon />
-            </div>
-            <div>
-                <BadgeMl />
-            </div>
-        </div>
-        )
-    }
-}
-
->>>>>>> 23d019b6b6bd6091dd2e8460a77a20a7ca563b37
 export default Categories;
